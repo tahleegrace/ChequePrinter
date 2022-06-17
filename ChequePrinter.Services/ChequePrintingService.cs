816171fcf6f01ff1f51513f36f0504617233ba13
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-17 19:04:42 +1000

    Added initial version of the cheque printing service.

---
+++ ChequePrinter.Services/ChequePrintingService.cs
@@ -0,0 +1,16 @@
+﻿namespace ChequePrinter.Services
+{
+    /// <summary>
+    /// The cheque printing service.
+    /// </summary>
+    public class ChequePrintingService : IChequePrintingService
+    {
+        /// <summary>
+        /// Prints the specified currency in text version (e.g. one hundred and twenty-three dollars).
+        /// </summary>
+        public string Print(decimal value)
+        {
+            return "Not implemented.";
+        }
+    }
+}
---
+++ ChequePrinter.Services/IChequePrintingService.cs
@@ -0,0 +1,13 @@
+﻿namespace ChequePrinter.Services
+{
+    /// <summary>
+    /// A service that prints cheques.
+    /// </summary>
+    public interface IChequePrintingService
+    {
+        /// <summary>
+        /// Prints the specified currency in text version (e.g. one hundred and twenty-three dollars).
+        /// </summary>
+        string Print(decimal value);
+    }
+}
