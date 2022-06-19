452c768bd135868c0b68b2136b23b5ca54d219aa
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-19 13:57:04 +1000

    Added dependency setup to ChequePrinter.Services library.

---
+++ ChequePrinter.Services/DependencySetup.cs
@@ -0,0 +1,13 @@
+﻿namespace ChequePrinter.Services
+{
+    /// <summary>
+    /// Provides dependency setup for the ChequePrinter.Services library.
+    /// </summary>
+    public static class DependencySetup
+    {
+        public static void AddChequePrinterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IChequePrintingService, ChequePrintingService>();
+        }
+    }
+}
